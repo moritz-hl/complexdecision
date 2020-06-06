bcc7820ea12cbf3122e1ec738a9376237b6a95dd
Moritz Hartlieb <moritz-hartlieb@web.de>
2020-06-06 15:59:53 +0200

    test

--- app.py
+++ app.py
@@ -11,7 +11,7 @@ def homepage():
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
     <p>Formula: {getp}</p>
-    """.format(time=the_time)
+    """.format(time=the_time, getp = getp)
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
 
