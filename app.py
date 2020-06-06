e1211c4442e6c7201252d99ddb8d815718d60552
Moritz Hartlieb <moritz-hartlieb@web.de>
2020-06-06 15:55:49 +0200

    test

--- app.py
+++ app.py
@@ -8,8 +8,6 @@ def homepage():
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
-
-    <img src="http://loremflickr.com/600/400" />
     """.format(time=the_time)
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
