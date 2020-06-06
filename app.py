2316372e0a647629094c018ecf81e83071578cd0
Moritz Hartlieb <moritz-hartlieb@web.de>
2020-06-06 15:58:28 +0200

    test

--- app.py
+++ app.py
@@ -1,13 +1,16 @@
 from flask import Flask
+from flask import request
 from datetime import datetime
 app = Flask(__name__)
 
 @app.route('/')
 def homepage():
+    getp = request.args.get("formula")
     the_time = datetime.now().strftime("%A, %d %b %Y %l:%M %p")
     return """
     <h1>Hello heroku</h1>
     <p>It is currently {time}.</p>
+    <p>Formula: {getp}</p>
     """.format(time=the_time)
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
