b00df3d95ac1466f6927b1d9f17324cc99b33200
Moritz Hartlieb <moritz-hartlieb@web.de>
2020-06-07 15:43:07 +0200

    style tests

--- app.py
+++ app.py
@@ -31,25 +31,29 @@ calc_grammar = r"""
 """
 
 
-header = """
+header = r"""
 <!DOCTYPE html>
 <html>
 <head>
 <title>Gröbner Basis</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
 <link rel = "stylesheet" href = "./style.css">
 </head>
 <body>
+<div id = "wrapper">
 """
 
-footer = """
+footer = r"""
 <h2>Examples</h2>
 <ul>
 <li> <a href = "/?formula=x+%3D+2+%2F%5C+y+%3D+3+%3D%3D>+x+%2By+%3D+5">2+3 = 5</a>
 <li> <a href = "/?formula=a%5E2+%3D+2+%2F%5C+x%5E2%2Ba*x+%2B+1+%3D+0+%3D%3D>+x%5E4+%2B+1+%3D+0">Complex Roots</a>
 <li> <a href = "/?formula=a*x%5E2%2Bb*x%2Bc+%3D+0+%2F%5C+a*y%5E2%2Bb*y%2Bc+%3D+0+%2F%5C+1-%28x-y%29*z+%3D+0+%3D%3D>+a*x*y+%3D+c">Vieta I</a>
 <li> <a href = "/?formula=a*x%5E2%2Bb*x%2Bc+%3D+0+%2F%5C+a*y%5E2%2Bb*y%2Bc+%3D+0+%2F%5C+1-%28x-y%29*z+%3D+0+%3D%3D>+a*%28x%2By%29+%3D+-b">Vieta II</a>
+<li> <a href = "/?formula=a*c+%3D+1%2Bb+%2F%5C+b*d+%3D+1%2Bc+%2F%5C+c*e+%3D+1%2Bd+%2F%5C+d*f+%3D+1%2Be+%2F%5C+a*q%3D1+%2F%5C+b*w+%3D+1+%2F%5C+c*r+%3D+1+%2F%5C+d*t+%3D+1+%2F%5C+e*p+%3D+1+%2F%5C+f*l+%3D+1+%3D%3D>+a+%3D+f">Period 5</a>
 <li> <a href = "/?formula=x+%3D+0++%3D%3D>++x+%3D+1">non-sense</a>
 </ul>
+</div>
 </body>
 </html>
 """
@@ -108,20 +112,20 @@ def homepage():
         suc_class = "true"
         if not ev:
             suc_class = "false"
-        return wrap("""
-        <h1>Gröbner</h1>
-        <div id = "wrapper" class = "{cls}">
+        return wrap(f"""
+        <h1>&#x2200;a, b, c &isin; &#x2102;</h1>
+        <div id = "result" class = "{suc_class}">
         <form action = "/">
-        <input style="width:800px" type = "text" id = "formula" name = "formula" value = "{raw}">
+        <input style="width:800px" type = "text" id = "formula" name = "formula" value = "{formula}">
         </form>
-        <p>Formula: {getp}</p>
+        <p>Formula: {parsed}</p>
         <p> Gröbner-Basis: {basis}</p>
         <p>Wahr: {ev} </p>
         </div>
-        """.format(raw = formula, getp = parsed, basis = basis, ev = ev, cls = suc_class))
+        """)
     except Exception as err:
         return wrap("""
-        <h1>An error occured</h1>
+        <h1>Parser-Error</h1>
         <form action = "/">
         <input style="width:800px" type = "text" id = "formula" name = "formula" value = "{raw}">
         </form>
