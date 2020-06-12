fc9cf566485262679f2c59637ffca7b86d717d5a
Moritz Hartlieb <moritz-hartlieb@web.de>
2020-06-12 20:32:39 +0200

    refactor

--- app.py
+++ app.py
@@ -4,75 +4,9 @@ from datetime import datetime
 from sympy import symbols, groebner
 from lark import Lark, Transformer, v_args
 
+from static import *
+
 app = Flask(__name__, static_url_path = "", static_folder="static")
-
-header = r"""
-<!DOCTYPE html>
-<html>
-<head>
-<title>Gröbner Basis</title>
-<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
-<link rel = "stylesheet" href = "./style.css">
-</head>
-<body>
-<div id = "wrapper">
-<h1>&#x2102;omplex Decisions</h1>
-"""
-
-footer = r"""
-<h2>Examples</h2>
-<ul>
-<li> <a href = "/?formula=x+%3D+2+%2F%5C+y+%3D+3+%3D%3D>+x+%2B+y+%3D+5">2+3 = 5</a>
-<li> <a href = "/?formula=a%5E2+%3D+2+%2F%5C+x%5E2%2Ba*x+%2B+1+%3D+0+%3D%3D>+x%5E4+%2B+1+%3D+0">Complex Roots</a>
-<li> <a href = "/?formula=a*x%5E2%2Bb*x%2Bc+%3D+0+%2F%5C+a*y%5E2%2Bb*y%2Bc+%3D+0+%2F%5C+x+%2F%3D+y+%3D%3D>+a*x*y+%3D+c">Vieta I</a>
-<li> <a href = "/?formula=a*x%5E2%2Bb*x%2Bc+%3D+0+%2F%5C+a*y%5E2%2Bb*y%2Bc+%3D+0+%2F%5C+x+%2F%3D+y+%3D%3D>+a*%28x%2By%29+%3D+-b">Vieta II</a>
-<li> <a href = "/?formula=a*c+%3D+1%2Bb+%2F%5C+b*d+%3D+1%2Bc+%2F%5C+c*e+%3D+1%2Bd+%2F%5C+d*f+%3D+1%2Be+%2F%5C+a+%2F%3D+0+%2F%5C+b+%2F%3D+0+%3D%3D>+a+%3D+f">Period = 5</a>
-<li> <a href = "/?formula=a*x+%3D+0+%3D%3D>+a+%3D+0+%5C%2F+x+%3D+0">Integral I</a>
-<li> <a href = "/?formula=a*x+%3D+0++%2F%5C+a+%2F%3D+0+%3D%3D>+x+%3D+0">Integral II</a>
-<li> <a href = "/?formula=x%5E5%2Bx%5E4%2B1%3D0+%3D%3D>+x%5E2%2Bx%2B1+%3D+0+%5C%2F+x%5E3-x%2B1+%3D+0">Factor </a>
-<li> <a href = "/?formula=x+%3D+0++%3D%3D>++x+%3D+1">non-sense</a>
-
-</ul>
-</div>
-<script src = "app.js"></script>
-</body>
-</html>
-"""
-
-
-def wrap(content):
-    return header + content + footer
-
-
-
-
-calc_grammar = r"""
-    imp:  conj "==>" disj -> imp
-    conj: [rel ("/\\" rel)*] -> conj
-    disj: [eq ("\\/" eq)*] -> disj
-    ?rel : ineq
-        | eq
-    ?ineq: term "/=" term -> ineq
-    ?eq: term "=" term -> eq
-    ?term: sum
-    ?sum: product
-        | sum "+" product   -> add
-        | sum "-" product   -> sub
-    ?product: atom
-        | product "*" atom  -> mul
-        | product "/" num  -> div
-    ?atom: NUMBER           -> number
-         | "-" atom         -> neg
-         | NAME             -> var
-         | "(" sum ")"
-         | atom "^" num  -> power
-    ?num: NUMBER           -> number
-    %import common.CNAME -> NAME
-    %import common.NUMBER
-    %import common.WS_INLINE
-    %ignore WS_INLINE
-"""
-
 
 @v_args(inline=True)
 class UnivTree(Transformer):
@@ -122,11 +56,11 @@ def neg(tm, z):
 def homepage():
     transformer = UnivTree()
 
-    univ_parser = Lark(calc_grammar, parser='lalr', transformer=transformer, start = "imp")
+    univ_parser = Lark(open("grammar.lark"), parser='lalr', transformer=transformer, start = "imp")
 
     formula = request.args.get("formula")
 
-    if formula == "":
+    if not formula:
         formula = "x=y==>y=x"
     
     try:
@@ -142,28 +76,10 @@ def homepage():
         suc_class = "true"
         if not ev:
             suc_class = "false"
-        return wrap(f"""
-       
-<div id = "result" class = "{suc_class}">
-<form action = "/">
-    <div>
-<div id = "quant">&#x2200; <span id = "qvars"></span> .
-</div>
-<div id = "input"><input type = "text" id = "formula" name = "formula" value = "{formula}"></div>
-</div>
-</form>
-<p> Gröbner-Basis: {basis}</p>
-<h1>{ev}</h1>
-</div>
-        """)
+        return pos_out(suc_class, formula, basis, ev)
+
     except Exception as err:
-        return wrap("""
-        <form action = "/">
-        <input style="width:800px" type = "text" id = "formula" name = "formula" value = "{raw}">
-        </form>
-        <p>
-        {err}
-        <p>
-        """.format(raw = formula, err = err))
+        return err_out(formula, err)
+
 if __name__ == '__main__':
     app.run(debug=True, use_reloader=True)
---
+++ static.py
@@ -0,0 +1,61 @@
+
+header = r"""
+<!DOCTYPE html>
+<html>
+<head>
+<title>Gröbner Basis</title>
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.0/css/bootstrap.min.css" integrity="sha384-9aIt2nRpC12Uk9gS9baDl411NQApFmC26EwAOH8WgZl5MYYxFfc+NcPb1dKGj7Sk" crossorigin="anonymous">
+<link rel = "stylesheet" href = "./style.css">
+</head>
+<body>
+<div id = "wrapper">
+<h1>&#x2102;omplex Decisions</h1>
+"""
+
+footer = r"""
+<h2>Examples</h2>
+<ul>
+<li> <a href = "/?formula=x+%3D+2+%2F%5C+y+%3D+3+%3D%3D>+x+%2B+y+%3D+5">2+3 = 5</a>
+<li> <a href = "/?formula=a%5E2+%3D+2+%2F%5C+x%5E2%2Ba*x+%2B+1+%3D+0+%3D%3D>+x%5E4+%2B+1+%3D+0">Complex Roots</a>
+<li> <a href = "/?formula=a*x%5E2%2Bb*x%2Bc+%3D+0+%2F%5C+a*y%5E2%2Bb*y%2Bc+%3D+0+%2F%5C+x+%2F%3D+y+%3D%3D>+a*x*y+%3D+c">Vieta I</a>
+<li> <a href = "/?formula=a*x%5E2%2Bb*x%2Bc+%3D+0+%2F%5C+a*y%5E2%2Bb*y%2Bc+%3D+0+%2F%5C+x+%2F%3D+y+%3D%3D>+a*%28x%2By%29+%3D+-b">Vieta II</a>
+<li> <a href = "http://localhost:5000/?formula=a*c+%3D+1%2Bb+%2F%5C+b*d+%3D+1%2Bc+%2F%5C+c*e+%3D+1%2Bd+%2F%5C+d*f+%3D+1%2Be+%2F%5C+a+%2F%3D+0+%2F%5C+b+%2F%3D+0+%2F%5C+c%2F%3D+0+%2F%5C+d+%2F%3D+0%3D%3D%3E+a+%3D+f">Period = 5</a>
+<li> <a href = "/?formula=a*x+%3D+0+%3D%3D>+a+%3D+0+%5C%2F+x+%3D+0">Integral I</a>
+<li> <a href = "/?formula=a*x+%3D+0++%2F%5C+a+%2F%3D+0+%3D%3D>+x+%3D+0">Integral II</a>
+<li> <a href = "/?formula=x%5E5%2Bx%5E4%2B1%3D0+%3D%3D>+x%5E2%2Bx%2B1+%3D+0+%5C%2F+x%5E3-x%2B1+%3D+0">Factor </a>
+<li> <a href = "/?formula=x+%3D+0++%3D%3D>++x+%3D+1">non-sense</a>
+
+</ul>
+</div>
+<script src = "app.js"></script>
+</body>
+</html>
+"""
+
+def wrap(content):
+    return header + content + footer
+
+def pos_out(suc_class, formula, basis, ev):
+    return wrap(f"""
+<div id = "result" class = "{suc_class}">
+<form action = "/">
+    <div>
+<div id = "quant">&#x2200; <span id = "qvars"></span> .
+</div>
+<div id = "input"><input type = "text" id = "formula" name = "formula" value = "{formula}"></div>
+</div>
+</form>
+<p> Gröbner-Basis: {basis}</p>
+<h1>{ev}</h1>
+</div>
+        """)
+
+def err_out(formula, err):
+    return wrap("""
+        <form action = "/">
+        <input style="width:800px" type = "text" id = "formula" name = "formula" value = "{raw}">
+        </form>
+        <p>
+        {err}
+        <p>
+        """.format(raw = formula, err = err))
